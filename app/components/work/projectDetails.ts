55e99053380ce726b6252da9da4a4eeabc59fe6f
Esteban Soto <joanestebansr@gmail.com>
2023-08-20 12:50:16 -0600

    my first commit

---
+++ app/components/work/projectDetails.ts
@@ -0,0 +1,199 @@
+import {IconType} from "react-icons";
+
+export type ProjectProps = {
+  id: number;
+  name: string;
+  description: string;
+  technologies: IconType[];
+  techNames: string[];
+  techLinks: string[];
+  github: string;
+  demo: string;
+  image: string;
+  available: boolean;
+};
+
+export const projects = [
+    {
+        id: 0,
+        name: "Viernes 19 de Mayo",
+        description:
+            "Primera clase, se vieron conceptos generales del curso y se conversó un poco acerca de los temas que se van a estar cubriendo.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 1,
+        name: "Viernes 26 de Mayo",
+        description:
+            "Realizamos un examen pequeño para dar ideas acerca de la filosofía. El profesor nos brinda conceptos e ideas acerca de la filosofía.El resto de la clase se basó en el profesor explicando diversos puntos acerca de la filosofía a nivel general.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 2,
+        name: "Viernes 2 de Junio",
+        description:
+            "Semana U, no hubo clase.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 3,
+        name: "Viernes 9 de Junio",
+        description:
+            "Clase virtual, correspondía realizar el primer análisis de película acerca de 'El mundo de Sofia'.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 4,
+        name: "Viernes 16 de Junio",
+        description:
+            "No hubo clase, el profesor de enfermó.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 5,
+        name: "Viernes 23 de Junio",
+        description:
+            "Clase presencial, Vimos la diferencia entre la etica y la moral, la moral se basa en los valores, los principios y creencias de la persona y la ética es el estudio de la moral. Se aclaran puntos sobre el proyecto.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 6,
+        name: "Viernes 30 de Junio",
+        description:
+            "Clase presencial. Vimos acerca de la filosofía de la ciencia o Epistemología.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 7,
+        name: "Viernes 7 de Julio",
+        description:
+            "Cierre de medio año.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 8,
+        name: "Viernes 14 de Julio",
+        description:
+            "Clase virtual. Análisis de película de 'El hombre bicentenario'.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 9,
+        name: "Viernes 21 de Julio",
+        description:
+            "Clase presencial. Se utilizó el espacio en clase para avanzar el trabajo de investigación. El profesor nos señaló varias correciones a realizar en el mismo.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 10,
+        name: "Viernes 28 de Julio",
+        description:
+            "Clase presencial. Exposiciones acerca del tema para foro de discusión. Tema correspondido Capitalismo/Comunismo Dictadura/Democracia. Debimos realizar también un comentario en classroom acerca de lo que entendimos.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 11,
+        name: "Viernes 4 de Agosto",
+        description:
+            "Clase presencial. Realizamos las presentaciones acerca de las lecturas asignadas. Lectura: aportes de la filosofía al pensamiento crítico. Entrega de infografía de la misma.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 12,
+        name: "Viernes 11 de Agosto",
+        description:
+            "Clase presencial, exposiciones acerca del trabajo de investigación.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+    {
+        id: 13,
+        name: "Viernes 18 de Agosto",
+        description:
+            "Clase virtual, de manera virtual continuamos con las exposiciones faltantes. Última clase del curso.",
+        technologies: [],
+        techNames: [],
+        techLinks: [],
+        github: "",
+        demo: "",
+        image: "/projects/portfolio.webp",
+        available: true,
+    },
+];
